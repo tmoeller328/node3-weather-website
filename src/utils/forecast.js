1054a58f90a9afef848413d80afe57b643b1bd05
Tom Moellering <512883@gmelogin.com>
2019-08-19 16:01:07 -0500

    add temp high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,9 @@ const forecast = (latitude, longitude, callback) => {
             callback(undefined, 
                 body.daily.data[0].summary + ' It is currently ' +
                 body.currently.temperature + ' degress out. There is a ' +
-                body.currently.precipProbability*100 + '% chance of rain.'
+                body.currently.precipProbability*100 + '% chance of rain. ' +
+                ' The high was ' + body.daily.data[0].temperatureHigh +
+                ' The low was ' + body.daily.data[0].temperatureLow
             )
         }
     })
